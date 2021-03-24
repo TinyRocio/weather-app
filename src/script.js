4fd44078f3a49218a50f71cf63a5e02078bdc796
TinyRocio <78389359+TinyRocio@users.noreply.github.com>
2021-03-24 18:01:40 -0400

    added conversion from Fahrenheit to Celsius

--- src/script.js
+++ src/script.js
@@ -34,6 +34,8 @@ function displayWeatherCondition(response){
   let currentDescriptionElement=document.querySelector("#current-description")
   let iconElement=document.querySelector("#icon");
 
+  fahrenheitTemperature=response.data.main.temp;
+
   cityElement.innerHTML=response.data.name;
   currentTemperatureElement.innerHTML=Math.round(response.data.main.temp);
   currentDescriptionElement.innerHTML=(response.data.weather[0].description);
@@ -91,6 +93,22 @@ function getCurrentLocation(event){
   navigator.geolocation.getCurrentPosition(searchLocation)
 }
 
+function displayFahrenheitTemperature(event){
+  event.preventDefault();
+  let temperatureElement =document.querySelector("#current-temperature");
+  temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
+}
+
+
+function displayCelsiusTemperature(event){
+  event.preventDefault();
+  let temperatureElement=document.querySelector("#current-temperature");
+  let celsiusTemperature=(5/9) * (fahrenheitTemperature - 32)
+  temperatureElement.innerHTML=Math.round(celsiusTemperature);
+}
+
+let fahrenheitTemperature = null;
+
 let dateElement = document.querySelector("#today");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
@@ -100,5 +118,11 @@ currentLocationButton.addEventListener("click", getCurrentLocation);
 
 let searchForm = document.querySelector("#search-engine-form");
 searchForm.addEventListener("submit", handleSubmit );
+
+let fahrenheitLink=document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink=document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("New York");
